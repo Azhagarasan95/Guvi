8cbbd4e46f30b7fe31496d9201a887700f8f6eb5
Azhagarasan95 <azhagarasan.azhagu@gmail.com>
2016-08-24 10:50:58 +0530

    Create FirstRepeatedElement.java

---
+++ FirstRepeatedElement.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class FirstRepeatedElement
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter size of array and the elements separated by space");
+        int size=input.nextInt();
+        int[] elements=new int[size];
+        for(int i=0;i<size;i++)
+            elements[i]=input.nextInt();
+        for(int i=0;i<size-1;i++)
+            for(int j=0;j<size;j++)
+                if(i!=j)
+                    if(elements[i] == elements[j])
+                        {
+                            System.out.print("The first repeated element is " + elements[i];
+                            break;
+                        }
+        input.close();
+    }
+}
