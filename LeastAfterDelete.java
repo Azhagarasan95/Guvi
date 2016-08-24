fc11368932e85e8acaca7af6c5ec81c970c2699f
Azhagarasan95 <azhagarasan.azhagu@gmail.com>
2016-08-24 14:36:45 +0530

    Update LeastAfterDelete.java

--- LeastAfterDelete.java
+++ LeastAfterDelete.java
@@ -7,25 +7,31 @@ public class LeastAfterDelete {
         String num=input.next();
         int k=input.nextInt();
         int len=num.length();
-        StringBuffer sb=new StringBuffer(num);
-        char[] arr=num.toCharArray();
-        for(int i=0;i<len-1;i++)
-        	for(int j=i+1;j<len;j++)
-        	{
-        		if(((int) arr[i])<((int) arr[j]))
-        		{
-        			char temp;
-        			temp=arr[i];
-        			arr[i]=arr[j];
-        			arr[j]=temp;
-        		}
-        	}
-        StringBuilder sbd=new StringBuilder(num);
-        for(int i=0;i<k;i++)
+        if(k<len)
         {
-        	sbd.deleteCharAt(sbd.toString().indexOf(arr[i]));
+        	StringBuffer sb=new StringBuffer(num);
+        	char[] arr=num.toCharArray();
+        	for(int i=0;i<len-1;i++)
+	        	for(int j=i+1;j<len;j++)
+        		{
+	        		if(((int) arr[i])<((int) arr[j]))
+        			{
+	        			char temp;
+        				temp=arr[i];
+        				arr[i]=arr[j];
+        				arr[j]=temp;
+        			}
+        		}
+        	StringBuilder sbd=new StringBuilder(num);
+        	for(int i=0;i<k;i++)
+        	{
+	        	sbd.deleteCharAt(sbd.toString().indexOf(arr[i]));
+	        }
+	        System.out.println(sbd.toString());
         }
-        System.out.println(sbd.toString());
+        else
+        	System.out.print("Error");
+        input.close();
 	}
 
 }
