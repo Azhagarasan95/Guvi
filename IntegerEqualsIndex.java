8b1037139cdebc459bda7f4ab747a8a022046279
Azhagarasan95 <azhagarasan.azhagu@gmail.com>
2016-08-24 10:43:48 +0530

    Update IntegerEqualsIndex.java

--- IntegerEqualsIndex.java
+++ IntegerEqualsIndex.java
@@ -6,13 +6,18 @@ class IntegerEqualsIndex
         Scanner input=new Scanner(System.in);
         System.out.print("Enter size of array ");
         int size=input.nextInt();
-        System.out.print("Enter " + size + " elements ");
-        int[] elements=new int[size];
-        for(int i=0;i<size;i++)
-            elements[i]=input.nextInt();
-        for(int i=0;i<size;i++)
-            if(elements[i] == i)
-                System.out.print(i + " ");
+        if(size<=1)
+            System.out.print("Error");
+        else
+        {
+            System.out.print("Enter " + size + " elements ");
+            int[] elements=new int[size];
+            for(int i=0;i<size;i++)
+                elements[i]=input.nextInt();
+            for(int i=0;i<size;i++)
+                if(elements[i] == i)
+                    System.out.print(i + " ");
+        }
         input.close();
     }
 }
